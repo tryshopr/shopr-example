cdbb0c1dc4fee8adaf92cb023d15b49d4c846c19
Artsiom Shut <artemshut@gmail.com>
2016-07-28 10:19:45 +0300

    [Product] display average rating

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,9 @@
 module ProductsHelper
+  DEFAULT_RATING = 3
+
+  def average_rating(object)
+    ratings = object.comments.pluck(:rating)
+    sum = ratings.inject(:+)
+    sum.present? ? sum / ratings.count : DEFAULT_RATING
+  end
 end
