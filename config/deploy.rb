76dee432aeb45494365ecf64d076a577e30cb448
Kiryl Ply <kiryl.ply@gmail.com>
2016-09-14 19:20:36 +0300

    devise token

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,8 @@ set :pty, true
 set :log_level, :info
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+# set :linked_files, %w{config/database.yml}
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w(bin log tmp/cache vendor/bundle public/system public/attachment public/uploads)
