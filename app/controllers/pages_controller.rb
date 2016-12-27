cb7362876c5fefd24b02c21e299475b35020d657
Kiryl Ply <craft37@gmail.com>
2016-12-27 18:14:35 +0300

    add reviews slider

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
 
   def home
     @products = Shopr::Product.active.featured.includes(:product_categories, :variants).root
+    @comments = Shopr::Comment.all.order('created_at DESC').limit(5)
   end
 
 end
